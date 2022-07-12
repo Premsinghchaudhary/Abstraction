6e6dffee293350ebbf3f33ff17c85f5fa7b625ed
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-07-12 13:28:31 +0530

    Add files via upload

---
+++ Abstract/use_of_abstration.java
@@ -0,0 +1,13 @@
+package Abstraction;
+
+public class use_of_abstration extends fib {
+
+	public static void main(String agrs[]) {
+		use_of_abstration obj=new use_of_abstration();
+		 obj.fib_seires();
+		 obj.sum_of_digits();
+		
+		
+	}
+
+}
